dc84c5410cf08a4a1241662303e8497ae722b178
Roman Tuomisto <roman.tuomisto@teliacompany.com>
2020-08-25 18:21:25 +0300

    create isAdmin middleware method

--- middleware/index.js
+++ middleware/index.js
@@ -1,16 +1,25 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
-var User=require("../models/user");
-var Support=require("../models/support");
-var Complaint=require("../models/complaints");
-var middlewareObj={};
+var User = require('../models/user');
+var Support = require('../models/support');
+var Complaint = require('../models/complaints');
+var middlewareObj = {};
 
-middlewareObj.isLoggedIn=function(req, res, next){
-    if(req.isAuthenticated()){
+middlewareObj.isLoggedIn = function (req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
-    req.flash("error","You must be logged in to do that");
-    res.redirect("/login");
-}
+    req.flash('error', 'You must be logged in to do that');
+    res.redirect('/login');
+};
 
-module.exports=middlewareObj;
+middlewareObj.isAdmin = (req, res, user, next) => {
+    if (req.isAuthenticated() && user.role === 'admin') {
+        return next();
+    } else {
+        req.flash('error', 'You are not allowed to view this page!');
+        return res.redirect('back');
+    }
+};
+
+module.exports = middlewareObj;
--- routes/auth.js
+++ routes/auth.js
@@ -1,52 +1,52 @@
-var express=require("express");
-var router=express.Router({mergeParams:true});
-var passport=require("passport");
-const User = require("../models/user");
+var express = require('express');
+var router = express.Router({ mergeParams: true });
+var passport = require('passport');
+const User = require('../models/user');
 
 // HOME ROUTE
-router.get("/",function(req,res){
-    res.render("home");
+router.get('/', function (req, res) {
+    res.render('home');
 });
 
 // REGISTER ROUTES
-router.get("/register",function(req,res){
-   res.render("auth/register");
+router.get('/register', function (req, res) {
+    res.render('auth/register');
 });
 
-router.post("/register",function(req,res){
-    var newUser=new User({
-        username:req.body.username,
-        firstName:req.body.firstName,
-        lastName:req.body.lastName,
-        email:req.body.email
+router.post('/register', function (req, res) {
+    var newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        role: req.body.role || 'user',
     });
-    User.register(newUser,req.body.password,function(err,user){
-        if(err){
-            req.flash("error","Something went wrong. Please Try Again.")
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            req.flash('error', 'Something went wrong. Please Try Again.');
             console.log(err);
-            res.redirect("back");
-        }else{
-            passport.authenticate("local")(req,res,function(){
-                req.flash("success","Thank You for registering to the Ticketing System "+user.username+"!");
-                res.redirect("/");
+            res.redirect('back');
+        } else {
+            passport.authenticate('local')(req, res, function () {
+                req.flash('success', 'Thank You for registering to the Ticketing System ' + user.username + '!');
+                res.redirect('/');
             });
         }
     });
 });
 
 // LOGIN ROUTES
-router.get("/login",function(req,res){
-    res.render("auth/login");
+router.get('/login', function (req, res) {
+    res.render('auth/login');
 });
 
-router.post("/login",function(req,res){
-    passport.authenticate("local",
-    {
-      successRedirect: "/",
-      failureRedirect: "/login",
-      failureFlash: true,
-      successFlash: "Welcome back, " + req.body.username + "!"
+router.post('/login', function (req, res) {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true,
+        successFlash: 'Welcome back, ' + req.body.username + '!',
     })(req, res);
 });
 
-module.exports=router;
+module.exports = router;
