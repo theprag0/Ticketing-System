be6a13b8bfd4760d5cc44fe887d5be1cc7129e63
Roman Tuomisto <roman.tuomisto@teliacompany.com>
2020-08-25 20:32:58 +0300

    add support panel link for admins

--- routes/support.js
+++ routes/support.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var router = express.Router({ mergeParams: true });
+var router = express.Router();
 var Complaint = require('../models/complaints');
 const emailServer = require('../utils/sendEmail');
 const middlewareObj = require('../middleware/index');
 
 // SUPPORT ROUTES
 // Show all complaints
-router.get('/support', middlewareObj.isAdmin, function (req, res) {
+router.get('/', middlewareObj.isAdmin, function (req, res) {
     Complaint.find({ archived: false }, function (err, foundComplaint) {
         if (err) {
             console.log(err);
@@ -17,61 +17,62 @@ router.get('/support', middlewareObj.isAdmin, function (req, res) {
 });
 
 //Show more info about a ticket
-router.get('/support/:id', middlewareObj.isAdmin, async (req, res) => {
-    try {
-        const foundComplaint = await Complaint.findById(req.params.id).populate('author');
-        if (!foundComplaint) {
+router
+    .get('/:id', middlewareObj.isAdmin, async (req, res) => {
+        try {
+            const foundComplaint = await Complaint.findById(req.params.id).populate('author');
+            if (!foundComplaint) {
+                req.flash('error', 'Something went wrong. Please try again');
+                return res.redirect('back');
+            }
+            if (foundComplaint.status === 'Pending') {
+                foundComplaint.status = 'Open';
+                foundComplaint.reviewStartedAt = Date.now();
+                const startedComplaint = await foundComplaint.save();
+                return res.render('support/show', { complaint: startedComplaint });
+            }
+            res.render('support/show', { complaint: foundComplaint });
+        } catch (err) {
             req.flash('error', 'Something went wrong. Please try again');
             return res.redirect('back');
         }
-        if (foundComplaint.status === 'Pending') {
-            foundComplaint.status = 'Open';
-            foundComplaint.reviewStartedAt = Date.now();
-            const startedComplaint = await foundComplaint.save();
-            return res.render('support/show', { complaint: startedComplaint });
+    })
+
+    // router.route("/support/:id").get(async(req, res, next) => {
+    //     try {
+    //     const complaint = await Complaint.findById(req.params.id).populate("author.id");
+    //     complaint.status = "Open";
+    //     const startedComplaint = await complaint.save();
+    //     res.render("support/show",{complaint:startedComplaint});
+    //     res.status(200).json({startedComplaint})
+    //     } catch(err) {
+    //     return next(err);
+    //     }
+    //     }).put(async(req, res, next) => {
+    //     try {
+    //     const complaint = await Complaint.findById(req.params.id).populate("author.id");
+    //     complaint.status = "Closed";
+    //     const startedComplaint = await complaint.save();
+    //     res.render("support/show",{complaint:startedComplaint});
+    //     res.status(200).json({startedComplaint})
+    //     } catch(err) {
+    //     return next(err);
+    //     }
+    //     });
+
+    // Add status by updating db
+    .put('/:id', middlewareObj.isAdmin, async (req, res) => {
+        try {
+            const foundComplaint = await Complaint.findById(req.params.id).populate('author');
+            foundComplaint.status = 'Close';
+            const resolvedComplaint = await foundComplaint.save();
+            emailServer.sendVerificartionEmail(resolvedComplaint);
+            req.flash('success', 'The user has been notified about the update!');
+            return res.redirect('back');
+        } catch (err) {
+            req.flash('error', 'Something went wrong. Please try again');
+            return res.redirect('back');
         }
-        res.render('support/show', { complaint: foundComplaint });
-    } catch (err) {
-        req.flash('error', 'Something went wrong. Please try again');
-        return res.redirect('back');
-    }
-});
-
-// router.route("/support/:id").get(async(req, res, next) => {
-//     try {
-//     const complaint = await Complaint.findById(req.params.id).populate("author.id");
-//     complaint.status = "Open";
-//     const startedComplaint = await complaint.save();
-//     res.render("support/show",{complaint:startedComplaint});
-//     res.status(200).json({startedComplaint})
-//     } catch(err) {
-//     return next(err);
-//     }
-//     }).put(async(req, res, next) => {
-//     try {
-//     const complaint = await Complaint.findById(req.params.id).populate("author.id");
-//     complaint.status = "Closed";
-//     const startedComplaint = await complaint.save();
-//     res.render("support/show",{complaint:startedComplaint});
-//     res.status(200).json({startedComplaint})
-//     } catch(err) {
-//     return next(err);
-//     }
-//     });
-
-// Add status by updating db
-router.put('/support/:id', middlewareObj.isAdmin, async (req, res) => {
-    try {
-        const foundComplaint = await Complaint.findById(req.params.id).populate('author');
-        foundComplaint.status = 'Close';
-        const resolvedComplaint = await foundComplaint.save();
-        emailServer.sendVerificartionEmail(resolvedComplaint);
-        req.flash('success', 'The user has been notified about the update!');
-        return res.redirect('back');
-    } catch (err) {
-        req.flash('error', 'Something went wrong. Please try again');
-        return res.redirect('back');
-    }
-});
+    });
 
 module.exports = router;
