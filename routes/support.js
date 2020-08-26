c82297fe93439281551376d59c3d8ff870f34f97
Praghadiesh <praghadieshnikhil234@gmail.com>
2020-08-26 22:40:07 +0530

    add response and resolve time feature

--- routes/support.js
+++ routes/support.js
@@ -85,4 +85,30 @@ router
         }
     });
 
-module.exports = router;
+function responseTime(a, b) {
+    var time=Math.floor((b-a)/60000);
+    if(time<=60){
+      return time + " mins";
+    }else if(time>60){
+        return time/60 + " hours"
+    }else if(time=0){
+        return time+ " mins, Lightning fast!! Employee Of the Month";
+    }
+};
+
+function resolveTime(a,b,c,d){
+    if(c==undefined || d==undefined){
+       c=0;
+       d=0; 
+    }
+    var openToClose=Math.floor((b-a)/60000);
+    var pendingToReopen=Math.floor((d-c)/60000);
+    var time=openToClose-pendingToReopen;
+    if(time<=60){
+        return time + " mins";
+      }else if(time>60){
+          return time/60 + " hours"
+      }
+}
+
+module.exports=router;
